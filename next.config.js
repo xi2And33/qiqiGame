d34dc61df7b97d4d92e9588cdbd28afa2106def4
Web3ILoveQier <13552265254@163.com>
2024-11-10 21:31:27 +0800

    ff

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
+// // next.config.js
+// module.exports = {
+//   output: 'export', // This tells Next.js to export your site as static HTML
+//   swcMinify: false,
+//   productionBrowserSourceMaps: true,
+//   images: {
+//     unoptimized: true, // Disable image optimization during the export
+//   },
+// };
+
 // next.config.js
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 module.exports = {
-  output: 'export', // This tells Next.js to export your site as static HTML
-  swcMinify: false,
-  productionBrowserSourceMaps: true,
-  images: {
-    unoptimized: true, // Disable image optimization during the export
-  },
+  output: 'export',
+  assetPrefix,
+  basePath,
 };
